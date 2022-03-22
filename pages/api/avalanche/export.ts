389696c2bd58b625a1e990d6f0b3cd41e461f8ea
Aminah Burch <hello@aminah.io>
2022-03-22 10:38:35 +0300

    Updated the export file to be able to send avax from xchain to cchain

--- pages/api/avalanche/export.ts
+++ pages/api/avalanche/export.ts
@@ -14,19 +14,32 @@ export default async function (
     const amount = '50000000';
 
     // Taking inspiration for xChain do the same for cChain
-    const [xChain, cChain] = [client.XChain(), undefined];
-    const [xKeychain, cKeychain] = [xChain.keyChain(), undefined];
-    const [xKeypair, cKeypair] = [xKeychain.importKey(secret), undefined];
-    const [xAddress, cAddress] = [xKeypair.getAddressString(), undefined];
+    const [xChain, cChain] = [client.XChain(), client.CChain()];
+    const [xKeychain, cKeychain] = [xChain.keyChain(), cChain.keyChain()];
+    const [xKeypair, cKeypair] = [
+      xKeychain.importKey(secret),
+      cKeychain.importKey(secret),
+    ];
+    const [xAddress, cAddress] = [
+      xKeypair.getAddressString(),
+      cKeypair.getAddressString(),
+    ];
 
     // Fetch UTXOs (unspent transaction outputs)
     const {utxos} = await xChain.getUTXOs(xAddress);
 
     // Get the real ID for the cChain
-    const chainId = undefined;
+    const chainId = cChain.getBlockchainID();
 
     // Prepare the export transaction from X -> C chain
-    const exportTx = await xChain.buildExportTx(undefined);
+    const exportTx = await xChain.buildExportTx(
+      utxos,
+      new BN(amount),
+      chainId,
+      [cAddress],
+      [xAddress],
+      [xAddress],
+    );
 
     // Sign and send the transaction
     const hash = await xChain.issueTx(exportTx.sign(xKeychain));
